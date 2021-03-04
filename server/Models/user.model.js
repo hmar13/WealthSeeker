cd0f19080c1095c275ce6c8eaf131e6d762d3992
Haydn <haydnmartin@gmx.com>
2021-03-04 16:35:49 +0100

    server update

--- server/Models/user.model.js
+++ server/Models/user.model.js
@@ -5,11 +5,11 @@ exports.checkUser = async (data) => {
   try {
     // console.log(data.username, data.userpassword)
     const result = await db.query(
-      `SELECT userid, username, userwatchlist
+      `SELECT id, username, userwatchlist
       FROM users
       WHERE (username = '${data.username}') AND (userpassword = '${data.userpassword}')`
     );
-    console.log(result.rows);
+    // console.log(result.rows);
     return result.rows;
   } catch (error) {
     console.error('Error getting username/password', error);
@@ -20,9 +20,9 @@ exports.checkUser = async (data) => {
 // Create a new User
 exports.postNewUser = (data) => {
   db.query(
-    `INSERT INTO users (userid, username, userpassword, useremail, userwatchlist)
-    VALUES ($1, $2, $3, $4, $5)`,
-    [data.userid, data.username, data.userpassword, data.useremail, data.userwatchList]
+    `INSERT INTO users (username, userpassword, useremail, userwatchlist)
+    VALUES ($1, $2, $3, $4)`,
+    [data.username, data.userpassword, data.useremail, data.userwatchList]
   );
 };
 
